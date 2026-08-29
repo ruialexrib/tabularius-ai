936d74d040eb5a6628e5b0ef95a029678295e21d
ruialexrib <5676648+ruialexrib@users.noreply.github.com>
2026-08-29 20:24:37 +0100

    feat: configure SAF-T ledger persistence

--- src/TabulariusAI.Web/Data/TabulariusDbContext.cs
+++ src/TabulariusAI.Web/Data/TabulariusDbContext.cs
@@ -3,9 +3,7 @@ using TabulariusAI.Web.Data.Entities;
 
 namespace TabulariusAI.Web.Data;
 
-/// <summary>
-/// Represents the local persistence context for accounting entities, dossiers and imports.
-/// </summary>
+/// <summary>Represents the local persistence context for accounting entities, dossiers and imports.</summary>
 public sealed class TabulariusDbContext(DbContextOptions<TabulariusDbContext> options) : DbContext(options)
 {
     /// <summary>Gets the accounting entities managed by the application.</summary>
@@ -22,6 +20,10 @@ public sealed class TabulariusDbContext(DbContextOptions<TabulariusDbContext> op
     public DbSet<SaftSupplier> SaftSuppliers => Set<SaftSupplier>();
     /// <summary>Gets the products and services preserved from SAF-T (PT) imports.</summary>
     public DbSet<SaftProduct> SaftProducts => Set<SaftProduct>();
+    /// <summary>Gets the accounting transactions preserved from SAF-T (PT) imports.</summary>
+    public DbSet<SaftTransaction> SaftTransactions => Set<SaftTransaction>();
+    /// <summary>Gets the debit and credit lines preserved from SAF-T (PT) accounting transactions.</summary>
+    public DbSet<SaftTransactionLine> SaftTransactionLines => Set<SaftTransactionLine>();
 
     /// <inheritdoc />
     protected override void OnModelCreating(ModelBuilder modelBuilder)
@@ -34,5 +36,7 @@ public sealed class TabulariusDbContext(DbContextOptions<TabulariusDbContext> op
         modelBuilder.Entity<SaftCustomer>(entity => { entity.Property(item => item.CustomerId).HasMaxLength(100).IsRequired(); entity.Property(item => item.AccountId).HasMaxLength(100).IsRequired(); entity.Property(item => item.TaxId).HasMaxLength(30).IsRequired(); entity.Property(item => item.CompanyName).HasMaxLength(300).IsRequired(); entity.HasOne(item => item.SaftImport).WithMany(item => item.Customers).HasForeignKey(item => item.SaftImportId).OnDelete(DeleteBehavior.Cascade); entity.HasIndex(item => new { item.SaftImportId, item.CustomerId }).IsUnique(); });
         modelBuilder.Entity<SaftSupplier>(entity => { entity.Property(item => item.SupplierId).HasMaxLength(100).IsRequired(); entity.Property(item => item.AccountId).HasMaxLength(100).IsRequired(); entity.Property(item => item.TaxId).HasMaxLength(30).IsRequired(); entity.Property(item => item.CompanyName).HasMaxLength(300).IsRequired(); entity.HasOne(item => item.SaftImport).WithMany(item => item.Suppliers).HasForeignKey(item => item.SaftImportId).OnDelete(DeleteBehavior.Cascade); entity.HasIndex(item => new { item.SaftImportId, item.SupplierId }).IsUnique(); });
         modelBuilder.Entity<SaftProduct>(entity => { entity.Property(item => item.ProductType).HasMaxLength(10).IsRequired(); entity.Property(item => item.ProductCode).HasMaxLength(100).IsRequired(); entity.Property(item => item.ProductGroup).HasMaxLength(100); entity.Property(item => item.ProductDescription).HasMaxLength(500).IsRequired(); entity.Property(item => item.ProductNumberCode).HasMaxLength(100).IsRequired(); entity.HasOne(item => item.SaftImport).WithMany(item => item.Products).HasForeignKey(item => item.SaftImportId).OnDelete(DeleteBehavior.Cascade); entity.HasIndex(item => new { item.SaftImportId, item.ProductCode }).IsUnique(); });
+        modelBuilder.Entity<SaftTransaction>(entity => { entity.Property(item => item.JournalId).HasMaxLength(100).IsRequired(); entity.Property(item => item.JournalDescription).HasMaxLength(500).IsRequired(); entity.Property(item => item.TransactionId).HasMaxLength(100).IsRequired(); entity.Property(item => item.SourceId).HasMaxLength(100).IsRequired(); entity.Property(item => item.Description).HasMaxLength(500).IsRequired(); entity.Property(item => item.DocArchivalNumber).HasMaxLength(100); entity.Property(item => item.TransactionType).HasMaxLength(10).IsRequired(); entity.Property(item => item.CustomerId).HasMaxLength(100); entity.Property(item => item.SupplierId).HasMaxLength(100); entity.HasOne(item => item.SaftImport).WithMany(item => item.Transactions).HasForeignKey(item => item.SaftImportId).OnDelete(DeleteBehavior.Cascade); entity.HasIndex(item => new { item.SaftImportId, item.TransactionId }).IsUnique(); entity.HasIndex(item => new { item.SaftImportId, item.TransactionDate }); entity.HasIndex(item => new { item.SaftImportId, item.JournalId }); });
+        modelBuilder.Entity<SaftTransactionLine>(entity => { entity.Property(item => item.RecordId).HasMaxLength(100).IsRequired(); entity.Property(item => item.AccountId).HasMaxLength(100).IsRequired(); entity.Property(item => item.SourceDocumentId).HasMaxLength(100); entity.Property(item => item.Description).HasMaxLength(500).IsRequired(); entity.Property(item => item.Side).HasMaxLength(1).IsFixedLength().IsRequired(); entity.Property(item => item.Amount).HasPrecision(19, 4); entity.HasOne(item => item.SaftTransaction).WithMany(item => item.Lines).HasForeignKey(item => item.SaftTransactionId).OnDelete(DeleteBehavior.Cascade); entity.HasIndex(item => new { item.SaftTransactionId, item.RecordId }).IsUnique(); entity.HasIndex(item => item.AccountId); });
     }
 }
