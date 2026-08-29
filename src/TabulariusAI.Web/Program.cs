3cf869a427d4cc97106c942c1b83e56e14f34f01
ruialexrib <5676648+ruialexrib@users.noreply.github.com>
2026-08-29 16:28:37 +0100

    feat(logging): configure copyable diagnostic logs

--- src/TabulariusAI.Web/Program.cs
+++ src/TabulariusAI.Web/Program.cs
@@ -1,8 +1,26 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using TabulariusAI.Web.Data;
 using TabulariusAI.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+var logDirectory = Path.Combine(builder.Environment.ContentRootPath, "logs");
+Directory.CreateDirectory(logDirectory);
+
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(builder.Configuration)
+    .Enrich.FromLogContext()
+    .Enrich.WithProperty("Application", "TabulariusAI")
+    .WriteTo.Console()
+    .WriteTo.File(
+        Path.Combine(logDirectory, "tabularius-.log"),
+        rollingInterval: RollingInterval.Day,
+        retainedFileCountLimit: 14,
+        outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {SourceContext} {Message:lj}{NewLine}{Exception}")
+    .CreateLogger();
+
+builder.Host.UseSerilog();
 
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<TabulariusDbContext>(options =>
@@ -10,6 +28,11 @@ builder.Services.AddDbContext<TabulariusDbContext>(options =>
 builder.Services.AddScoped<ISaftHeaderReader, SaftHeaderReader>();
 
 var app = builder.Build();
+
+app.UseSerilogRequestLogging(options =>
+{
+    options.MessageTemplate = "HTTP {RequestMethod} {RequestPath} responded {StatusCode} in {Elapsed:0.0000} ms";
+});
 
 if (!app.Environment.IsDevelopment())
 {
@@ -25,4 +48,17 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
-app.Run();
+try
+{
+    Log.Information("Starting Tabularius AI version {Version}", typeof(Program).Assembly.GetName().Version?.ToString());
+    app.Run();
+}
+catch (Exception exception)
+{
+    Log.Fatal(exception, "Tabularius AI terminated unexpectedly");
+    throw;
+}
+finally
+{
+    Log.CloseAndFlush();
+}
